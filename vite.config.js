a2d95d945c927337d97e89cd7277cb49c3837a3d
Towkir Ahmed <towkir17@gmail.com>
2023-06-03 20:30:10 +0600

    setup alias for path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import path from "path";
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			// these are the aliases and paths to them
+			'@': path.resolve('./src')
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
