5e63102113a39b9e162f8b69b3306836e3862c71
Towkir Ahmed <towkir17@gmail.com>
2023-06-06 03:25:54 +0600

    added status data and current user data to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,32 @@
 import { readable } from 'svelte/store';
+
+export const currentUser = readable({
+    image: './assets/user-images/image-zena.jpg',
+    name: 'Zena Kelley',
+    username: 'velvetround',
+});
 export const categories = readable([
     'UI',
     'UX',
     'Enhancement',
     'Bug',
-    'Feature'
-])
+    'Feature',
+]);
+
+export const statuses = readable([
+    {
+        name: 'Planned',
+        description: 'Ideas prioritized for research',
+        color: '#F49F85',
+    },
+    {
+        name: 'In-Progress',
+        description: 'Currently being developed',
+        color: '#AD1FEA',
+    },
+    {
+        name: 'Live',
+        description: 'Released features',
+        color: '#62BCFA',
+    },
+]);
