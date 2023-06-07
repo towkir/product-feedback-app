33e36b2a88ca7283e453c9440579c52c467a01aa
Towkir Ahmed <towkir17@gmail.com>
2023-06-07 06:05:30 +0600

    prepared an empty state homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
     import Sidebar from "@/components/Sidebar.svelte";
+    import Header from "@/components/Header.svelte";
+    import NoFeedbackFound from "@/components/feedback/NoFeedbackFound.svelte";
 </script>
 
 <div class="container">
     <Sidebar/>
+    <main>
+        <Header />
+        <NoFeedbackFound />
+    </main>
 </div>
 
 <style lang="stylus" global>
@@ -11,4 +17,8 @@
   .container
     width 1110px
     margin 0 auto
+    display flex
+    gap 30px
+    main
+      flex-grow 1
 </style>
