9231ab688799ce45af63de8f57d0989fb4b507b3
Towkir Ahmed <towkir17@gmail.com>
2023-06-20 05:46:22 +0600

    implemented a comments viewer

---
+++ src/components/comments/CommentsWrapper.svelte
@@ -0,0 +1,25 @@
+<script>
+    import CommentCard from "@/components/comments/CommentCard.svelte";
+    import {singularOrPlural} from "@/scripts/helper.js";
+
+    export let comments = [];
+</script>
+
+<div class="comments-wrapper">
+    <h3>{singularOrPlural(comments.length, 'Comment', 'Comments')}</h3>
+    {#each comments as comment}
+        <CommentCard comment="{comment}"/>
+    {/each}
+</div>
+
+<style lang="stylus">
+  .comments-wrapper
+    background-color brand-white
+    border-radius 10px
+    padding 24px 32px
+    h3
+      heading-3()
+      color brand-light-navy
+      &:not(:last-child)
+        margin-bottom 28px
+</style>
--- src/routes/feedback/view/[slug]/+page.svelte
+++ src/routes/feedback/view/[slug]/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import FeedbackCard from "@/components/feedback/FeedbackCard.svelte";
     import NavHeader from "@/components/NavHeader.svelte";
+    import CommentsWrapper from "@/components/comments/CommentsWrapper.svelte";
     export let data;
 </script>
 
 <div class="container">
     <NavHeader showEditButton="{true}"/>
     <FeedbackCard feedback="{data}" clickable="{false}" />
+    <CommentsWrapper comments="{data.comments}"/>
 </div>
 
 <style lang="stylus">
