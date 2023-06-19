7267c71e0ebd08b3d4888f059d4b9b19e14b84c2
Towkir Ahmed <towkir17@gmail.com>
2023-06-20 05:46:01 +0600

    added a comment card using message card

---
+++ src/components/comments/CommentCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {replies} from "@/store/store.js";
+    import MessageCard from "@/components/comments/MessageCard.svelte";
+    export let comment = {};
+    $: repliesOnThisComment = $replies.filter((item) => item.commentId === comment.id);
+</script>
+
+<MessageCard role="comment" message="{comment}" replies="{repliesOnThisComment}" />
+{#each repliesOnThisComment as reply, r}
+    <MessageCard role="reply" message="{reply}" isLastItem="{repliesOnThisComment.length === r + 1}"/>
+{/each}
