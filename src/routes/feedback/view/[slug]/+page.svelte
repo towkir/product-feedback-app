52999a0a95c8e8c7dfec39b08b8aedad17f1e8eb
Towkir Ahmed <towkir17@gmail.com>
2023-06-20 16:59:33 +0600

    added comment composer to feedback details page

--- src/routes/feedback/view/[slug]/+page.svelte
+++ src/routes/feedback/view/[slug]/+page.svelte
@@ -2,6 +2,7 @@
     import FeedbackCard from "@/components/feedback/FeedbackCard.svelte";
     import NavHeader from "@/components/NavHeader.svelte";
     import CommentsWrapper from "@/components/comments/CommentsWrapper.svelte";
+    import CommentCreateCard from "@/components/comments/CommentCreateCard.svelte";
     export let data;
 </script>
 
@@ -9,10 +10,13 @@
     <NavHeader showEditButton="{true}"/>
     <FeedbackCard feedback="{data}" clickable="{false}" />
     <CommentsWrapper comments="{data.comments}"/>
+    <CommentCreateCard />
 </div>
 
 <style lang="stylus">
     .container
       width 730px
       margin 0 auto
+      :global(.feedback-card)
+        margin-bottom 24px
 </style>
