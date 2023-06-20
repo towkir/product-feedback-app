8e89abfa43eada2701939ccc5427914a76e9dfbf
Towkir Ahmed <towkir17@gmail.com>
2023-06-20 16:58:18 +0600

    added a component handling comment and reply composing

---
+++ src/components/comments/CommentCreateCard.svelte
@@ -0,0 +1,19 @@
+<script>
+import MessageCreateCard from "@/components/comments/MessageCreateCard.svelte";
+</script>
+
+<div class="create-comment-wrapper">
+    <h3>Add Comment</h3>
+    <MessageCreateCard />
+</div>
+
+<style lang="stylus">
+  .create-comment-wrapper
+    background-color brand-white
+    border-radius 10px
+    padding 24px 32px
+    h3
+      heading-3()
+      color brand-light-navy
+      margin-bottom 24px
+</style>
