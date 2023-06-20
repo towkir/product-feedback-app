c8dfcf56c19d1c3da72b194d53c2e30819e4ad54
Towkir Ahmed <towkir17@gmail.com>
2023-06-20 16:02:13 +0600

    added a helper function to capitalize first character in a word

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -2,3 +2,7 @@ export function singularOrPlural(number, singular, plural) {
     const form = number === 1 ? singular : plural;
     return `${number} ${form}`;
 }
+
+export function capitalizeFirstChar(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
