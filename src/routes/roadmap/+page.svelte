ac8e32a88ed5bf26e7848e45c80d3d7c75eeb601
Towkir Ahmed <towkir17@gmail.com>
2023-07-21 13:06:59 +0600

    added responsive styles to roadmap page

--- src/routes/roadmap/+page.svelte
+++ src/routes/roadmap/+page.svelte
@@ -5,6 +5,11 @@
     import { statuses, feedbacks } from "@/store/store.js";
     const filteredStatuses = $statuses.filter(item => item.visible === true);
 
+    let activeColumn = 'In-Progress';
+
+    function setActiveColumn(status) {
+        activeColumn = status;
+    }
     function feedbacksOfStatus(status) {
         return $feedbacks.filter(item => item.status === status);
     }
@@ -25,7 +30,11 @@
     </nav>
     <div class="roadmap-header">
         {#each filteredStatuses as status}
-            <div class="column">
+            <div
+                class="column"
+                on:click={() => setActiveColumn(status.name)}
+                style="{activeColumn === status.name ? `border-bottom-color: ${status.color}` : ''}"
+            >
                 <h5>{status.name} ({feedbackCount(status.name)})</h5>
                 <p>{status.description}</p>
             </div>
@@ -33,7 +42,11 @@
     </div>
     <div class="roadmap-board">
         {#each filteredStatuses as status}
-            <div class="column">
+            <div class="column" class:active={activeColumn === status.name}>
+                <div class="column-header">
+                    <h3>{status.name} ({feedbackCount(status.name)})</h3>
+                    <p>{status.description}</p>
+                </div>
                 {#each feedbacksOfStatus(status.name) as feedback}
                     <FeedbackCard {feedback} view="grid" statusColor="{status.color}"/>
                 {/each}
@@ -57,6 +70,8 @@
       border-radius 10px
       margin-bottom 40px
       .left
+        h1
+          heading-1()
         .btn.link
           padding 0
           margin-bottom 5px
@@ -83,11 +98,44 @@
       display flex
       gap 30px
       .column
+        .column-header
+          display none
         flex-grow 1
         flex-shrink 1
         flex-basis 0
   @media screen and (max-width: 768px)
     .container
+      margin 56px auto
       .roadmap-board
         gap 10px
+  @media screen and (max-width: 690px)
+    .container
+      width 100%
+      margin 0
+      nav
+        border-radius 0
+        margin-bottom 8px
+      .roadmap-header
+        gap 0
+        margin-bottom 0
+        .column
+          padding 16px 6px
+          text-align center
+          border-bottom 4px solid transparent
+          p
+            display none
+      .roadmap-board
+        padding 24px
+        .column
+          .column-header
+            display block
+            margin-bottom 24px
+            h3
+              heading-1()
+              color brand-light-navy
+            p
+              body-1()
+              color brand-lighter-navy
+        .column:not(.active)
+          display none
 </style>
